bbfeba6797408ecd1aec25dcd3dcaeb2273d7453
snkly <800680+snkly@users.noreply.github.com>
2020-07-28 11:47:52 -0700

    Connect to data sources - SpaceX data api

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -2,8 +2,26 @@ require('dotenv').config();
 
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
+const { createStore } = require('./utils');
 
-const server = new ApolloServer({ typeDefs });
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const store = createStore();
+
+// If you use this.context in a datasource, 
+// it's critical to create a new instance in the dataSources function, 
+// rather than sharing a single instance. 
+// Otherwise, initialize might be called during 
+// the execution of asynchronous code for a particular user, 
+// replacing this.context with the context of another user.
+const server = new ApolloServer({
+  typeDefs,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store })
+  })
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
