775d2e91f5d47c4b286192ca92d87bac40ef5ffa
snkly <800680+snkly@users.noreply.github.com>
2020-07-28 13:32:13 -0700

    Added query resolvers: How GraphQL query fetches data

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,8 +1,7 @@
-require('dotenv').config();
-
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 const { createStore } = require('./utils');
+const resolvers = require('./resolvers');
 
 const LaunchAPI = require('./datasources/launch');
 const UserAPI = require('./datasources/user');
@@ -17,6 +16,7 @@ const store = createStore();
 // replacing this.context with the context of another user.
 const server = new ApolloServer({
   typeDefs,
+  resolvers,
   dataSources: () => ({
     launchAPI: new LaunchAPI(),
     userAPI: new UserAPI({ store })
