ff0554f3e408771493e5ff2c938d86320242048b
snkly <800680+snkly@users.noreply.github.com>
2020-07-29 16:22:13 -0700

    Update date with mutations: Added login page

--- start/client/src/pages/login.tsx
+++ start/client/src/pages/login.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import { useApolloClient, useQuery, useMutation, gql } from '@apollo/client'
 
+import { LoginForm, Loading } from '../components';
+import * as LoginTypes from './__generated__/login';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!) {
+    login(email: $email)
+  }
+`;
+
+/**
+ * Our useMutation hook returns a mutate function (login) 
+ * and the data object returned from the mutation that we destructure from the tuple. 
+ * Finally, we pass our login function to the LoginForm component.
+ */
 export default function Login() {
-  return <div />;
+  const [login, { data }] = useMutation<LoginTypes.login, LoginTypes.loginVariables>(LOGIN_USER);
+  return <LoginForm login={login} />;
 }
