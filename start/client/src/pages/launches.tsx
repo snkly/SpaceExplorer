3757a18150ef1bea0614f29a93ad1a18bcb2569b
snkly <800680+snkly@users.noreply.github.com>
2020-07-29 14:50:45 -0700

    Fetch data with queries: fetching a list of launchies

--- start/client/src/pages/launches.tsx
+++ start/client/src/pages/launches.tsx
@@ -1,6 +1,9 @@
 import React, { Fragment }  from 'react';
 import { RouteComponentProps } from '@reach/router';
-import { gql } from '@apollo/client'
+import { useQuery, gql } from '@apollo/client'
+
+import { LaunchTile, Header, Button, Loading } from '../components';
+import * as GetLaunchListTypes from './__generated__/GetLaunchList';
 
 export const LAUNCH_TILE_DATA = gql`
   fragment LaunchTile on Launch {
@@ -18,10 +21,65 @@ export const LAUNCH_TILE_DATA = gql`
   }
 `;
 
+/**
+ * Here, we're defining a query to fetch a list of launches 
+ * by calling the launches query from our schema.
+ * 
+ * The launches query returns an object type with a list of launches, 
+ * in addition to the cursor of the paginated list and 
+ * whether or not the list hasMore launches. 
+ * 
+ * We need to wrap the query with the gql function in order to parse it into an AST
+ */
+const GET_LAUNCHES = gql`
+  query launchList($after: String) {
+    launches(after: $after) {
+      cursor
+      hasMore
+      launches {
+        id
+        isBooked
+        rocket {
+          id
+          name
+        }
+        mission {
+          name
+          missionPatch
+        }
+      }
+    }
+  }
+`;
+
 interface LaunchesProps extends RouteComponentProps {}
 
+// Pass GET_LAUNCHES query to Apollo's useQuery component to render the list.
 const Launches: React.FC<LaunchesProps> = () => {
-  return <div />;
+  const { 
+    data, 
+    loading, 
+    error,
+    fetchMore
+  } = useQuery<
+    GetLaunchListTypes.GetLaunchList, 
+    GetLaunchListTypes.GetLaunchListVariables
+  >(GET_LAUNCHES);
+
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR</p>;
+  if (!data) return <p>Not found</p>;
+
+  return (
+    <Fragment>
+      <Header />
+      {data.launches &&
+        data.launches.launches &&
+        data.launches.launches.map((launch: any) => (
+          <LaunchTile key={launch.id} launch={launch} />
+        ))}
+    </Fragment>
+  );
 }
 
 export default Launches;
