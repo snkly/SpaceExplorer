b3d0175aa353767b25dac70708502ee1fcacf43e
snkly <800680+snkly@users.noreply.github.com>
2020-07-29 14:35:59 -0700

    Connected Apollo Client to React App

--- start/client/src/index.tsx
+++ start/client/src/index.tsx
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; 
+import Pages from './pages';
+import injectStyles from './styles';
 import { 
   ApolloClient, 
   ApolloProvider, 
@@ -6,20 +10,15 @@ import {
   gql 
 } from '@apollo/client';
 
-import React from 'react';
-import ReactDOM from 'react-dom'; 
-import Pages from './pages';
-import injectStyles from './styles';
-
-//import gql from 'graphql-tag';
-
 const cache = new InMemoryCache();
 
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   cache,
+  // uri that we pass in is the URL of the server
   uri: 'http://localhost:4000/'
 });
 
+// client.query(), Query our graph's API
 client
   .query({
     query: gql`
@@ -36,7 +35,15 @@ client
   .then(result => console.log(result));
 
 injectStyles();
-ReactDOM.render(
+
+/**
+ * Connecting Apollo Client to our React app 
+ * allows us to easily bind GraphQL operations to our UI.
+ * The ApolloProvider component is similar to React’s context provider. 
+ * It wraps your React app and places the client on the context, 
+ * which allows you to access it from anywhere in your component tree.
+ */
+ ReactDOM.render(
   <ApolloProvider client={client}>
     <Pages />
   </ApolloProvider>, 
